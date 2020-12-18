eee342942249b9d38bf65d4d06e89e4699e3ed7d
sgnoo <dn2757@gmail.com>
2020-12-18 10:35:39 +0900

    Make ButtonStep component

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
     <mobile-header-container v-if="$mq === 'mobile'" />
     <header-container v-else />
     <dropdown :items="['Abstain', 'Yes', 'No']" :hint="'Your choice'" style="margin-left: 60px;" @on-selected="select" />
-    <button-comp :name="'primary'" :status="''" :type="'primary'" style="margin-top:120px; margin-left: 16px;"></button-comp>
+    <button-comp-step :name="'PREV NAME'" :status="''" :type="'prev'" style="margin-top: 120px; margin-left: 16px;"></button-comp-step>
+    <button-comp-step :name="'NEXT NAME'" :status="''" :type="'next'" style="margin-top: 16px; margin-left: 16px;"></button-comp-step>
+    <button-comp :name="'primary'" :status="''" :type="'primary'" style="margin-top: 16px; margin-left: 16px;"></button-comp>
     <button-comp :name="'primary'" :status="'running'" :type="'primary'" style="margin-top:16px; margin-left: 16px;"></button-comp>
     <button-comp :name="'primary disabled'" :status="'disabled'" :type="'primary'" style="margin-top:16px; margin-left: 16px;"></button-comp>
     <button-comp :name="'secondary'" :status="''" :type="'secondary'" style="margin-top:16px; margin-left: 16px;"></button-comp>
@@ -12,6 +14,7 @@
     <button-comp :name="'vote'" :status="''" :type="'vote'" style="margin-top:16px; margin-left: 16px;"></button-comp>
     <button-comp :name="'vote'" :status="'running'" :type="'vote'" style="margin-top:16px; margin-left: 16px;"></button-comp>
     <button-comp :name="'vote disabled'" :status="'disabled'" :type="'vote'" style="margin-top:16px; margin-left: 16px;"></button-comp>
+    <button-comp :name="'hide'" :type="'hide'" style="margin-top:16px; margin-left: 16px;"></button-comp>
     <router-view />
     <mobile-footer-container v-if="$mq === 'mobile'" />
     <footer-container v-else />
@@ -25,6 +28,7 @@ import MobileHeader from '@/containers/MobileHeader.vue';
 import MobileFooter from '@/containers/MobileFooter.vue';
 import Dropdown from '@/components/Dropdown.vue';
 import Button from '@/components/Button.vue';
+import ButtonStep from '@/components/ButtonStep.vue';
 
 export default {
   name: 'App',
@@ -35,6 +39,7 @@ export default {
     'mobile-footer-container': MobileFooter,
     'dropdown': Dropdown,
     'button-comp': Button,
+    'button-comp-step': ButtonStep,
   },
   methods: {
     select (item) {
