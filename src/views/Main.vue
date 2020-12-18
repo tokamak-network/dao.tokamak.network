db44bd9af0233cc9d2d407a47ed6be0a2dad90a5
cd4761 <cd4761@gmail.com>
2020-12-18 15:27:21 +0900

    add example of text-input

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="main">
-    Tokamak network
+    <text-input :type="'email'" />
+    <text-input :type="'email'" :required="true" />
+    <text-input :type="'email'" tooltip="'hi'" />
+    <text-input :status="'disabled'" />
   </div>
 </template>
 
 <script>
+import TextInput from '@/components/TextInput.vue';
+
 export default {
+  components: {
+    'text-input': TextInput,
+  },
 };
 </script>
 
