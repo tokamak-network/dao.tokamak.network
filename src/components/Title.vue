764a019bc106c1d9ae858ce78b3f5378f561de5b
cd4761 <cd4761@gmail.com>
2020-12-18 15:44:33 +0900

    make Card Title

---
+++ src/components/Title.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="title">
+    {{ title }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
+
+<style>
+title {
+  height: 32px;
+  margin: 0 0px 12px 0;
+  font-family: Roboto;
+  font-size: 24px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.33;
+  letter-spacing: normal;
+  text-align: left;
+  color: #3e495c;
+}
+</style>
