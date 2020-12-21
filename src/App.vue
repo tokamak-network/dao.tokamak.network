ca0c7d3d218ddc53118375854166ff1a44fe3c70
sgnoo <dn2757@gmail.com>
2020-12-21 10:11:43 +0900

    Make modal

--- src/App.vue
+++ src/App.vue
@@ -2,28 +2,18 @@
   <div id="app">
     <mobile-header-container v-if="$mq === 'mobile'" />
     <header-container v-else />
-    <div style="display: flex; margin-top: 60px;">
-      <dropdown :items="['Abstain', 'Yes', 'No']" :hint="'Your choice'" style="margin-left: 60px;" :button-type="'a'" :selector-type="'a'" @on-selected="select" />
-      <dropdown :items="['Abstain', 'Yes', 'No']" :hint="'Your choice'" style="margin-left: 60px;" :button-type="'a'" :selector-type="'b'" @on-selected="select" />
-      <dropdown :items="['Abstain', 'Yes', 'No']" :hint="'Your choice'" style="margin-left: 60px;" :type="'a'" :disabled="true" @on-selected="select" />
-    </div>
-    <div>
-      <dropdown :items="['Abstain', 'Yes', 'No']" :hint="'Your choice'" style="margin-top: 120px; margin-left: 60px;" :button-type="'b'" :selector-type="'a'" @on-selected="select" />
-      <dropdown :items="['Abstain', 'Yes', 'No']" :hint="'Your choice'" style="margin-top: 120px; margin-left: 60px;" :button-type="'b'" :selector-type="'b'" @on-selected="select" />
-      <dropdown :items="['Abstain', 'Yes', 'No']" :hint="'Your choice'" style="margin-top: 120px; margin-left: 60px;" :type="'b'" :disabled="true" @on-selected="select" />
-    </div>
-    <button-comp-step :name="'PREV NAME'" :status="''" :type="'prev'" style="margin-top: 120px; margin-left: 16px;"></button-comp-step>
-    <button-comp-step :name="'NEXT NAME'" :status="''" :type="'next'" style="margin-top: 16px; margin-left: 16px;"></button-comp-step>
-    <button-comp :name="'primary'" :status="''" :type="'primary'" style="margin-top: 16px; margin-left: 16px;"></button-comp>
-    <button-comp :name="'primary'" :status="'running'" :type="'primary'" style="margin-top:16px; margin-left: 16px;"></button-comp>
-    <button-comp :name="'primary disabled'" :status="'disabled'" :type="'primary'" style="margin-top:16px; margin-left: 16px;"></button-comp>
-    <button-comp :name="'secondary'" :status="''" :type="'secondary'" style="margin-top:16px; margin-left: 16px;"></button-comp>
-    <button-comp :name="'secondary'" :status="'running'" :type="'secondary'" style="margin-top:16px; margin-left: 16px;"></button-comp>
-    <button-comp :name="'secondary disabled'" :status="'disabled'" :type="'secondary'" style="margin-top:16px; margin-left: 16px;"></button-comp>
-    <button-comp :name="'vote'" :status="''" :type="'vote'" style="margin-top:16px; margin-left: 16px;"></button-comp>
-    <button-comp :name="'vote'" :status="'running'" :type="'vote'" style="margin-top:16px; margin-left: 16px;"></button-comp>
-    <button-comp :name="'vote disabled'" :status="'disabled'" :type="'vote'" style="margin-top:16px; margin-left: 16px;"></button-comp>
-    <button-comp :name="'hide'" :type="'hide'" style="margin-top:16px; margin-left: 16px;"></button-comp>
+    <button-comp :name="'SHOW MODAL'" :type="'primary'" @on-clicked="show()" />
+    <modal-comp v-if="showModal"
+                :width="150"
+                @on-closed="showModal=false"
+    >
+      <template #body>
+        <!-- container -->
+        <div>
+          hi!
+        </div>
+      </template>
+    </modal-comp>
     <router-view />
     <mobile-footer-container v-if="$mq === 'mobile'" />
     <footer-container v-else />
@@ -35,9 +25,8 @@ import Header from '@/containers/Header.vue';
 import Footer from '@/containers/Footer.vue';
 import MobileHeader from '@/containers/MobileHeader.vue';
 import MobileFooter from '@/containers/MobileFooter.vue';
-import Dropdown from '@/components/Dropdown.vue';
 import Button from '@/components/Button.vue';
-import ButtonStep from '@/components/ButtonStep.vue';
+import Modal from '@/components/Modal.vue';
 
 export default {
   name: 'App',
@@ -46,13 +35,20 @@ export default {
     'footer-container': Footer,
     'mobile-header-container': MobileHeader,
     'mobile-footer-container': MobileFooter,
-    'dropdown': Dropdown,
     'button-comp': Button,
-    'button-comp-step': ButtonStep,
+    'modal-comp': Modal,
+  },
+  data () {
+    return {
+      showModal: false,
+    };
   },
   methods: {
     select (item) {
       alert(item);
+    },
+    show () {
+      this.showModal = true;
     },
   },
 };
