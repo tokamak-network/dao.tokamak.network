889fb2c526747c61c01b2a6f49c971a2f7b5ed71
sgnoo <dn2757@gmail.com>
2020-12-22 14:29:21 +0900

    Make table component

---
+++ src/containers/CardSupporters.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="card-vote">
+    <card-container :title="'Supporters'">
+      <template #body>
+        <supporters-table class="supporters-table" />
+      </template>
+    </card-container>
+  </div>
+</template>
+
+<script>
+import Card from '@/components/Card.vue';
+import Table from '@/components/Table.vue';
+
+export default {
+  components: {
+    'card-container': Card,
+    'supporters-table': Table,
+  },
+};
+</script>
+
+<style scoped>
+.supporters-table {
+  padding-top: 8px;
+  padding-bottom: 8px;
+}
+</style>
--- src/views/Election.vue
+++ src/views/Election.vue
@@ -4,6 +4,7 @@
       here
     </div>
     <div class="card-container">
+      <card-supporters />
       <card-rank :title="'Your Votes'" />
       <card-rank :title="'Rank'" />
       <card-resource />
@@ -13,25 +14,14 @@
 
 <script>
 import CardResource from '@/containers/CardResource.vue';
+import CardSupporters from '@/containers/CardSupporters.vue';
 import CardRank from '@/containers/CardRank.vue';
 
 export default {
   components: {
     'card-rank': CardRank,
     'card-resource': CardResource,
-  },
-  data () {
-    return {
-      your: [
-        { 'title': 'Claimable TON', 'content': '100 TON' },
-        { 'title': '# of Voted', 'content': '7 Polls' },
-      ],
-      committee: [
-        { 'title': 'Claimable TON', 'content': '100 TON' },
-        { 'title': '# of Voted', 'content': '7 Polls' },
-        { 'title': '% of winning vote lottery', 'content': '1.5 %' },
-      ],
-    };
+    'card-supporters': CardSupporters,
   },
 };
 </script>
