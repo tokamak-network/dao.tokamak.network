45ba6bb65188fb0a6748814ced582dbc739cd95c
cd4761 <cd4761@gmail.com>
2020-12-22 16:43:10 +0900

    make card for committee

---
+++ src/components/CardCommittee.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="card-committee">
+    <slot name="committee-body" />
+  </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style>
+.card-committee {
+  width: 786px;
+  padding: 25px 0 25px 30px;
+  margin-bottom: 20px;
+  border-radius: 10px;
+  box-shadow: 0 1px 1px 0 rgba(96, 97, 112, 0.16);
+  background-color: #ffffff;
+}
+</style>
