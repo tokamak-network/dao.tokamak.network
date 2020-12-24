16c1bb87c7df173dd0bccae8de6318843f54a91a
cd4761 <cd4761@gmail.com>
2020-12-24 14:45:41 +0900

    adopt hide button

--- src/containers/AgendaSlot.vue
+++ src/containers/AgendaSlot.vue
@@ -4,41 +4,73 @@
     <div class="agenda-info">
       {{ numAgenda }} Agendas - POSTED DEC 7, 2020, 16:00 UTC
     </div>
-    <card-agenda-slot v-for="agenda in agendas" :key="agenda.index" :agenda="agenda" />
+    <card-agenda-slot v-for="agenda in openAgendas" :key="agenda.timestamp" :agenda="agenda" />
+    <button-comp v-if="hide===false" :name="hideButton" :type="'hide'" @on-clicked="hideSection"></button-comp>
+    <div v-if="hide===true">
+      <card-agenda-slot v-for="agenda in hideAgendas" :key="agenda.timestamp" :agenda="agenda" />
+    </div>
   </div>
 </template>
 
 <script>
 import CardAgendaSlot from '@/components/CardAgendaSlot.vue';
+import Button from '@/components/Button.vue';
+
 export default {
   components: {
     'card-agenda-slot': CardAgendaSlot,
+    'button-comp': Button,
   },
   data (){
     return {
       agendas: [
         { 'index': 0, 'address': '0xEA8e2eC08dCf4971bdcdfFFe21439995378B44F3', 'name': '1000 TON to 0xaaabc', 'timestamp': 1608693812, 'claimableTon': 1000, 'choice': 'Not yet', 'voted': false, 'executed': false, 'claim': false, 'expired': false },
-        { 'index': 1, 'address': '0xEA8e2eC08dCf4971bdcdfFFe21439995378B44F3', 'name': '1000 TON to 0xaaabc', 'timestamp': 1608693812, 'claimableTon': 1000, 'choice': 'Yes', 'voted': false, 'executed': false, 'claim': false, 'expired': false },
-        { 'index': 2, 'address': '0xEA8e2eC08dCf4971bdcdfFFe21439995378B44F3', 'name': '1000 TON to 0xaaabc', 'timestamp': 1608693812, 'claimableTon': 1000, 'choice': 'Yes', 'voted': true, 'executed': false, 'claim': false, 'expired': false },
-        { 'index': 3, 'address': '0xEA8e2eC08dCf4971bdcdfFFe21439995378B44F3', 'name': '1000 TON to 0xaaabc', 'timestamp': 1608693812, 'claimableTon': 1000, 'choice': 'Yes', 'voted': true, 'executed': true, 'claim': false, 'expired': false },
-        { 'index': 4, 'address': '0xEA8e2eC08dCf4971bdcdfFFe21439995378B44F3', 'name': '1000 TON to 0xaaabc', 'timestamp': 1608693812, 'claimableTon': 1000, 'choice': 'Yes', 'voted': true, 'executed': true, 'claim': true, 'expired': false },
-        { 'index': 5, 'address': '0xEA8e2eC08dCf4971bdcdfFFe21439995378B44F3', 'name': '1000 TON to 0xaaabc', 'timestamp': 1608693812, 'claimableTon': 1000, 'choice': 'Yes', 'voted': true, 'executed': true, 'claim': true, 'expired': true },
-        { 'index': 6, 'address': '0xEA8e2eC08dCf4971bdcdfFFe21439995378B44F3', 'name': '1000 TON to 0xaaabc', 'timestamp': 1606793012, 'claimableTon': 1000, 'choice': 'Yes', 'voted': true, 'executed': true, 'claim': true, 'expired': true },
-        { 'index': 7, 'address': '0xEA8e2eC08dCf4971bdcdfFFe21439995378B44F3', 'name': '1000 TON to 0xaaabc', 'timestamp': 1606793012, 'claimableTon': 1000, 'choice': 'Yes', 'voted': true, 'executed': true, 'claim': true, 'expired': true },
-        { 'index': 8, 'address': '0xEA8e2eC08dCf4971bdcdfFFe21439995378B44F3', 'name': '1000 TON to 0xaaabc', 'timestamp': 1606793012, 'claimableTon': 1000, 'choice': 'Yes', 'voted': true, 'executed': true, 'claim': true, 'expired': true },
-        { 'index': 9, 'address': '0xEA8e2eC08dCf4971bdcdfFFe21439995378B44F3', 'name': '1000 TON to 0xaaabc', 'timestamp': 1606793012, 'claimableTon': 1000, 'choice': 'Yes', 'voted': true, 'executed': true, 'claim': true, 'expired': true },
-        { 'index': 10, 'address': '0xEA8e2eC08dCf4971bdcdfFFe21439995378B44F3', 'name': '1000 TON to 0xaaabc', 'timestamp': 1606793012, 'claimableTon': 1000, 'choice': 'Yes', 'voted': true, 'executed': true, 'claim': true, 'expired': true },
-        { 'index': 11, 'address': '0xEA8e2eC08dCf4971bdcdfFFe21439995378B44F3', 'name': '1000 TON to 0xaaabc', 'timestamp': 1606793012, 'claimableTon': 1000, 'choice': 'Yes', 'voted': true, 'executed': true, 'claim': true, 'expired': true },
-        { 'index': 12, 'address': '0xEA8e2eC08dCf4971bdcdfFFe21439995378B44F3', 'name': '1000 TON to 0xaaabc', 'timestamp': 1606793012, 'claimableTon': 1000, 'choice': 'Yes', 'voted': true, 'executed': true, 'claim': true, 'expired': true },
-        { 'index': 13, 'address': '0xEA8e2eC08dCf4971bdcdfFFe21439995378B44F3', 'name': '1000 TON to 0xaaabc', 'timestamp': 1606793012, 'claimableTon': 1000, 'choice': 'Yes', 'voted': true, 'executed': true, 'claim': true, 'expired': true },
-        { 'index': 14, 'address': '0xEA8e2eC08dCf4971bdcdfFFe21439995378B44F3', 'name': '1000 TON to 0xaaabc', 'timestamp': 1606793012, 'claimableTon': 1000, 'choice': 'Yes', 'voted': true, 'executed': true, 'claim': true, 'expired': true },
-        { 'index': 15, 'address': '0xEA8e2eC08dCf4971bdcdfFFe21439995378B44F3', 'name': '1000 TON to 0xaaabc', 'timestamp': 1606793012, 'claimableTon': 1000, 'choice': 'Yes', 'voted': true, 'executed': true, 'claim': true, 'expired': true },
+        { 'index': 1, 'address': '0xEA8e2eC08dCf4971bdcdfFFe21439995378B44F3', 'name': '1000 TON to 0xaaabc', 'timestamp': 1608693813, 'claimableTon': 1000, 'choice': 'Yes', 'voted': false, 'executed': false, 'claim': false, 'expired': false },
+        { 'index': 2, 'address': '0xEA8e2eC08dCf4971bdcdfFFe21439995378B44F3', 'name': '1000 TON to 0xaaabc', 'timestamp': 1608693814, 'claimableTon': 1000, 'choice': 'Yes', 'voted': true, 'executed': false, 'claim': false, 'expired': false },
+        { 'index': 3, 'address': '0xEA8e2eC08dCf4971bdcdfFFe21439995378B44F3', 'name': '1000 TON to 0xaaabc', 'timestamp': 1608693815, 'claimableTon': 1000, 'choice': 'Yes', 'voted': true, 'executed': true, 'claim': false, 'expired': false },
+        { 'index': 4, 'address': '0xEA8e2eC08dCf4971bdcdfFFe21439995378B44F3', 'name': '1000 TON to 0xaaabc', 'timestamp': 1608693816, 'claimableTon': 1000, 'choice': 'Yes', 'voted': true, 'executed': true, 'claim': true, 'expired': false },
+        { 'index': 5, 'address': '0xEA8e2eC08dCf4971bdcdfFFe21439995378B44F3', 'name': '1000 TON to 0xaaabc', 'timestamp': 1608693817, 'claimableTon': 1000, 'choice': 'Yes', 'voted': true, 'executed': true, 'claim': true, 'expired': true },
+        { 'index': 6, 'address': '0xEA8e2eC08dCf4971bdcdfFFe21439995378B44F3', 'name': '1000 TON to 0xaaabc', 'timestamp': 1606793018, 'claimableTon': 1000, 'choice': 'Yes', 'voted': true, 'executed': true, 'claim': true, 'expired': true },
+        { 'index': 7, 'address': '0xEA8e2eC08dCf4971bdcdfFFe21439995378B44F3', 'name': '1000 TON to 0xaaabc', 'timestamp': 1606793019, 'claimableTon': 1000, 'choice': 'Yes', 'voted': true, 'executed': true, 'claim': true, 'expired': true },
+        { 'index': 8, 'address': '0xEA8e2eC08dCf4971bdcdfFFe21439995378B44F3', 'name': '1000 TON to 0xaaabc', 'timestamp': 1606793022, 'claimableTon': 1000, 'choice': 'Yes', 'voted': true, 'executed': true, 'claim': true, 'expired': true },
+        { 'index': 9, 'address': '0xEA8e2eC08dCf4971bdcdfFFe21439995378B44F3', 'name': '1000 TON to 0xaaabc', 'timestamp': 1606793032, 'claimableTon': 1000, 'choice': 'Yes', 'voted': true, 'executed': true, 'claim': true, 'expired': true },
+        { 'index': 10, 'address': '0xEA8e2eC08dCf4971bdcdfFFe21439995378B44F3', 'name': '1000 TON to 0xaaabc', 'timestamp': 1606793042, 'claimableTon': 1000, 'choice': 'Yes', 'voted': true, 'executed': true, 'claim': true, 'expired': true },
+        { 'index': 11, 'address': '0xEA8e2eC08dCf4971bdcdfFFe21439995378B44F3', 'name': '1000 TON to 0xaaabc', 'timestamp': 1606793052, 'claimableTon': 1000, 'choice': 'Yes', 'voted': true, 'executed': true, 'claim': true, 'expired': true },
+        { 'index': 12, 'address': '0xEA8e2eC08dCf4971bdcdfFFe21439995378B44F3', 'name': '1000 TON to 0xaaabc', 'timestamp': 1606793062, 'claimableTon': 1000, 'choice': 'Yes', 'voted': true, 'executed': true, 'claim': true, 'expired': true },
+        { 'index': 13, 'address': '0xEA8e2eC08dCf4971bdcdfFFe21439995378B44F3', 'name': '1000 TON to 0xaaabc', 'timestamp': 1606793072, 'claimableTon': 1000, 'choice': 'Yes', 'voted': true, 'executed': true, 'claim': true, 'expired': true },
+        { 'index': 14, 'address': '0xEA8e2eC08dCf4971bdcdfFFe21439995378B44F3', 'name': '1000 TON to 0xaaabc', 'timestamp': 1606793082, 'claimableTon': 1000, 'choice': 'Yes', 'voted': true, 'executed': true, 'claim': true, 'expired': true },
+        { 'index': 15, 'address': '0xEA8e2eC08dCf4971bdcdfFFe21439995378B44F3', 'name': '1000 TON to 0xaaabc', 'timestamp': 1606793092, 'claimableTon': 1000, 'choice': 'Yes', 'voted': true, 'executed': true, 'claim': true, 'expired': true },
       ],
+      openAgendas: [],
+      hideAgendas: [],
+      hideButton: '',
+      hide: false,
     };
   },
   computed: {
     numAgenda (){
       return this.agendas.length;
+    },
+  },
+  created () {
+    this.cassify();
+  },
+  methods: {
+    cassify () {
+      if (this.agendas.length > 5) {
+        this.openAgendas = this.agendas.slice(0, 5);
+        this.hideAgendas = this.agendas.slice(5, this.agendas.length);
+        this.hideButton = 'View more Committee (' + this.hideAgendas.length + ')';
+      } else {
+        this.openAgendas = this.agendas;
+      }
+    },
+    hideSection () {
+      if (this.hide === false) {
+        this.hide = true;
+      } else {
+        this.hide = false;
+      }
     },
   },
 };
