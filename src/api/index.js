35ca23df98c3bc64fab60812c9c2630b65b1a09c
sgnoo <dn2757@gmail.com>
2021-01-27 17:26:39 +0900

    Update apis

--- src/api/index.js
+++ src/api/index.js
@@ -6,12 +6,13 @@ function createInstance () {
   });
 }
 const instance = createInstance();
+const chainId = 4;
 
 // TODO: check param
 export async function getRecentEvents () {
   const res = await instance.get('/events', {
     params: {
-      chainId: 4,
+      chainId,
       page: 1,
       pagesize: 20,
       eventNames: 'AgendaVoteCasted,AgendaExecuted,AgendaCreated',
@@ -23,7 +24,7 @@ export async function getRecentEvents () {
 export async function getCandidates () {
   const res = await instance.get('/layer2s/dao_candidates', {
     params: {
-      chainId: 4,
+      chainId,
     },
   });
   return res.data.datas;
@@ -32,7 +33,7 @@ export async function getCandidates () {
 export async function getAgendaVoteCasted () {
   const res = await instance.get('/events', {
     params: {
-      chainId: 4,
+      chainId,
       page: 1,
       pagesize: 100,
       eventNames: 'AgendaVoteCasted',
@@ -44,14 +45,18 @@ export async function getAgendaVoteCasted () {
 export async function getAgendas () {
   const res = await instance.get('/agendas', {
     params: {
-      chainId: 4,
+      chainId,
     },
   });
   return res.data.datas;
 }
 
 export async function getVotersByCandidate (layer2) {
-  const res = await instance.get(`/balances/stakeof?layer2=${layer2}`);
+  const res = await instance.get(`/balances/stakeof?layer2=${layer2}`, {
+    params: {
+      chainId,
+    },
+  });
   if (!res || res.data.datas.length === 0) {
     return [];
   }
@@ -59,15 +64,28 @@ export async function getVotersByCandidate (layer2) {
 }
 
 export async function getCandidateRankByVotes () {
-  const res = await instance.get('/balances/sums');
+  const res = await instance.get('/balances/sums', {
+    params: {
+      chainId,
+    },
+  });
   return res.data.datas;
 }
 
 export async function createAgenda (from, txHash, contents) {
-  await instance.post('/agendacontents', { account: from, tx: txHash, contents });
+  await instance.post('/agendacontents', { account: from, tx: txHash, contents }, {
+    params: {
+      chainId,
+    },
+  });
 }
 
 export async function getAgendaContents (agendaId) {
-  const res = await instance.get(`/agendacontents?chainId=4&agendaid=${agendaId}`);
+  const res = await instance.get('/agendacontents', {
+    params: {
+      chainId,
+      agendaid: agendaId,
+    },
+  });
   return res.data.datas[0];
 }
