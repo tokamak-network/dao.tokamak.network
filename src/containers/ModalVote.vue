584e169ad406eb60e93a0548bc844fb1cbf26801
Zena-park <zena.p@onther.io>
2021-02-01 15:29:19 +0900

    fix. findCandidateContractByOperator In the case of an operator with two layer2s
    In the case of an operator with two layer2s, voting proceeds by finding a member contract

--- src/containers/ModalVote.vue
+++ src/containers/ModalVote.vue
@@ -92,22 +92,40 @@ export default {
         this.choice = 0;
       }
     },
-    findCandidateContractByOperator (){
+    async findCandidateContractByOperator (){
       let _candidateContract = null;
+      let returnCandidateContract = null;
       console.log('findCandidateContractByOperator', this.candidates );
       if(this.candidates!=null && this.candidates.length > 0 ){
         for(let i=0; i< this.candidates.length; i++){
-          if( this.candidates[i].operator.toLowerCase() === this.account.toLowerCase() ) _candidateContract = this.candidates[i].candidateContract;
+          if(_candidateContract==null) _candidateContract=[];
+          if( this.candidates[i].operator.toLowerCase() === this.account.toLowerCase() ) _candidateContract.push(this.candidates[i].candidateContract) ;
         }
       }
-      return _candidateContract;
+      if(_candidateContract!=null && _candidateContract.length===1) returnCandidateContract=_candidateContract[0];
+      else if(_candidateContract!=null){
+        console.log('this.members', this.members) ;
+        if(this.members!=null && this.members.length> 0){
+          for(let i=0; i< _candidateContract.length ; i++ ){
+            for(let j=0; j< this.members.length ; j++){
+              if(_candidateContract[i].toLowerCase() === this.members[j].candidateContract.toLowerCase()){
+                console.log('compare', _candidateContract[i], this.members[j].candidateContract) ;
+                return _candidateContract[i];
+              }
+            }
+          }
+        }
+      }
+      return returnCandidateContract;
     },
     async vote () {
       if(this.web3==null) {
         alert('Check Connect Wallet!');
         return;
       }
-      const candidateContract = this.findCandidateContractByOperator();
+      const candidateContract = await this.findCandidateContractByOperator();
+      console.log('castVote candidateContract ==>>  ', candidateContract) ;
+
       /*
       const committeeProxy = getContract('DAOCommitteeProxy', this.web3);
       // const daoCommittee = getContract('Candidate', this.web3);
@@ -127,6 +145,9 @@ export default {
           alert('This Agenda is not in a state to vote.');
           this.close();
         }else{
+
+          console.log('castVote this.account ', this.account ) ;
+          console.log('castVote ',  this.id, this.choice, this.comment) ;
           const Candidate = new this.web3.eth.Contract(candidate.abi, candidateContract);
           if(Candidate!=null){
             await Candidate.methods.castVote(
@@ -137,9 +158,12 @@ export default {
               from: this.account,
               // gasLimit: Math.floor(gasLimit * 1.2),
             }).on('transactionHash', async (hash) => {
+              console.log('castVote transactionHash ', hash) ;
+
               this.$store.commit('SET_PENDING_TX', hash);
               this.close();
-            }).on('receipt', () => {
+            }).on('receipt', (receipt) => {
+              console.log('castVote receipt ',  receipt) ;
               this.$store.dispatch('setAgendas');
               this.$store.commit('SET_PENDING_TX', '');
               this.close();
