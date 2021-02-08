ebc89a8f81bcab15139dbcc9a039e0c8ce4e7295
sgnoo <dn2757@gmail.com>
2021-02-08 14:42:39 +0900

    Set design if EOA have candidate more than 2

--- src/containers/CardStats.vue
+++ src/containers/CardStats.vue
@@ -2,17 +2,46 @@
   <div class="card-stats">
     <card-container :title="'Your Stats'">
       <template #body>
-        <div v-if="clength > 1" class="stats">
-          <div class="candidate">{{ candidateContract }} </div>
+        <div>
+          <!-- <div class="stats">
+            <div class="candidate">{{ candidateContract }} </div>
+          </div> -->
+          <div class="stats">
+            <div class="title">Claimable TON</div>
+            <div class="content">{{ claimableAmount }} </div>
+          </div>
+          <div class="stats">
+            <div class="title"># of Agendas</div>
+            <div class="content">{{ totalAgendas }} Agendas</div>
+          </div>
         </div>
-        <div class="stats">
-          <div class="title">Claimable TON</div>
-          <div class="content">{{ claimableAmount }} </div>
-        </div>
-        <div class="stats">
-          <div class="title"># of Agendas</div>
-          <div class="content">{{ totalAgendas }} Agendas</div>
-        </div>
+
+        <!-- TODO: zena implement -->
+        <!-- <div v-for="(candidate, _, index) in candidates"
+             v-else
+             :key="candidate.candidateContract"
+        >
+          <div v-if="index !== 0" class="divide" />
+          <div class="container">
+            <div class="address">{{ candidateContract | hexSlicer }}</div>
+            <div class="stats-container">
+              <div class="stats-label">
+                Claimable TON
+              </div>
+              <div class="stats-value">
+                {{ claimableAmount }}
+              </div>
+            </div>
+            <div class="stats-container">
+              <div class="stats-label">
+                # of Voted
+              </div>
+              <div class="stats-value">
+                {{ totalAgendas }} Agendas
+              </div>
+            </div>
+          </div>
+        </div> -->
       </template>
     </card-container>
   </div>
@@ -60,7 +89,7 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .card-stats {
 }
 
@@ -115,4 +144,72 @@ export default {
   text-align: right;
   color: #2a72e5;
 }
+
+.container {
+  height: 125px;
+
+  display: flex;
+  flex-direction: column;
+  justify-items: center;
+  justify-content: center;
+}
+
+.address {
+  font-family: Roboto;
+  font-size: 15px;
+  font-weight: 500;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.73;
+  letter-spacing: normal;
+  text-align: left;
+  color: #3e495c;
+
+  margin-bottom: 5px;
+
+  padding-left: 20px;
+}
+
+.stats-container {
+  display: flex;
+  padding-left: 20px;
+  padding-right: 20px;
+
+  margin-top: 10px;
+
+  .stats-label {
+    flex: 1;
+
+    font-family: Roboto;
+    font-size: 15px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.73;
+    letter-spacing: normal;
+    text-align: left;
+    color: #86929d;
+  }
+  .stats-value {
+    font-family: Roboto;
+    font-size: 15px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.73;
+    letter-spacing: normal;
+    text-align: right;
+    color: #2a72e5;
+  }
+}
+
+.divide {
+  width: 100%;
+  height: 1px;
+  background: #dfe4ee;
+
+  padding-left: 20px;
+  padding-right: 20px;
+}
+
 </style>
--- src/containers/CardVote.vue
+++ src/containers/CardVote.vue
@@ -3,10 +3,26 @@
     <card-container :title="'Your Vote'">
       <template #body>
         <div class="vote-percentage-container">
-          <div v-if="clength > 1" class="vote-percentage">{{ candidateContract }}</div>
+          <!-- <div class="vote-percentage">{{ candidateContract }}</div> -->
           <div class="vote-percentage">{{ voteRates }}% of Agenda is voted </div>
           <vote-poll :pct="parseInt(voteRates)" :margin="16" />
         </div>
+
+        <!-- TODO: zena implement -->
+        <!-- <div v-for="(candidate, _, index) in candidates"
+             v-else
+             :key="candidate.candidateContract"
+        >
+          <div v-if="index > 0" class="divide" />
+          <div class="vote-container">
+            <div class="vote-info-container">
+              <div class="vote-address">{{ candidateContract | hexSlicer }}</div>
+              <span class="vote-percent">{{ voteRates }}</span>
+              <span class="vote-explanation"> % of Agenda is voted</span>
+            </div>
+            <vote-poll :pct="parseInt(voteRates)" :margin="16" />
+          </div>
+        </div> -->
       </template>
     </card-container>
   </div>
@@ -49,7 +65,7 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .card-vote {
 }
 
@@ -71,5 +87,71 @@ export default {
   padding-left: 20px;
   /* padding-right: 16px; */
   margin: 10px 0 25px;
+}
+
+
+.vote-container {
+  height: 89px;
+
+  display: flex;
+  flex-direction: column;
+
+  justify-items: center;
+  justify-content: center;
+
+  .vote-info-container {
+    display: flex;
+    padding-left: 20px;
+    padding-right: 20px;
+
+    margin-bottom: 15px;
+
+    .vote-address {
+      font-family: Roboto;
+      font-size: 14px;
+      font-weight: 500;
+      font-stretch: normal;
+      font-style: normal;
+      letter-spacing: normal;
+      text-align: left;
+      color: #3e495c;
+
+      flex: 1;
+
+      margin: 0px;
+    }
+
+    .vote-percent {
+      font-family: Roboto;
+      font-size: 13px;
+      font-weight: 500;
+      font-stretch: normal;
+      font-style: normal;
+      letter-spacing: normal;
+      text-align: right;
+      color: #2a72e5;
+
+      white-space: pre-wrap;
+    }
+
+    .vote-explanation {
+      font-family: Roboto;
+      font-size: 13px;
+      font-weight: normal;
+      font-stretch: normal;
+      font-style: normal;
+      letter-spacing: normal;
+      text-align: right;
+      color: #3e495c;
+
+      white-space: pre-wrap;
+    }
+  }
+}
+
+.divide {
+  width: 100%;
+  height: 1px;
+  background: #dfe4ee;
 }
 </style>
