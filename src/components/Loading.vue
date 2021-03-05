ac432d8368db4e0cfb6ca21af413e9ca71bcb622
sgnoo <dn2757@gmail.com>
2021-03-05 22:05:33 +0900

    Make loading component

---
+++ src/components/Loading.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="loading-spinner">
+    <div class="spinner" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scpoed>
+.loading-spinner {
+  min-height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.spinner {
+  border: 4px solid #f3f3f3; /* Light grey */
+  border-top: 4px solid #3498db; /* Blue */
+  border-radius: 50%;
+  width: 30px;
+  height: 30px;
+  animation: spin 2s linear infinite;
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+</style>
--- src/views/Agenda.vue
+++ src/views/Agenda.vue
@@ -90,6 +90,9 @@
         </div>
       </div>
     </div>
+    <div v-else>
+      <loading />
+    </div>
   </div>
 </template>
 
@@ -97,6 +100,7 @@
 import { agendaStatus, agendaResult } from '@/utils/helpers';
 import { mapGetters, mapState } from 'vuex';
 
+import Loading from '@/components/Loading.vue';
 import Dropdown from '@/components/Dropdown.vue';
 import Button from '@/components/Button.vue';
 import CardAgendaSlot from '@/containers/CardAgenda.vue';
@@ -107,6 +111,7 @@ import CardStatsCommittee from '@/containers/CardStatsCommittee.vue';
 
 export default {
   components: {
+    'loading': Loading,
     'dropdown': Dropdown,
     'hide-button': Button,
     'card-agenda': CardAgendaSlot,
