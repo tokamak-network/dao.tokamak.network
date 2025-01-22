e80891d071009e944436f9172cdc7071b53ae374
cd4761 <cd4761@gmail.com>
2025-01-22 17:22:03 +0900

    fix error

--- graphql/apollo.js
+++ graphql/apollo.js
@@ -1,6 +1,5 @@
 import ApolloClient from 'apollo-boost';
 
 export default new ApolloClient({
-  // uri: 'https://gateway.thegraph.com/api/1f84fcb975b81f061e36fb12b6e60475/subgraphs/id/CJLiXNdHXJ22BzWignD62gohDRVTYXJQVgU4qKJEtNVS',
   uri: process.env.VUE_APP_SUBGRAPH_DEV_API,
 });
