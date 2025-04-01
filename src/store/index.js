8f7b1a59a59205ec57d6fe37af321723a51c7387
cd4761 <cd4761@gmail.com>
2025-04-01 12:27:27 +0900

    remove console

--- src/store/index.js
+++ src/store/index.js
@@ -254,9 +254,7 @@ export default new Vuex.Store({
       const seigManager = getContract('SeigManager', state.web3);
 
       const createAgendaFee = await agendaManager.methods.createAgendaFees().call();
-      // const claimableAmount = await committeeProxy.methods.getClaimableActivityReward(state.account).call();
       const minimumAmount = await seigManager.methods.minimumAmount().call();
-      // console.log(claimableAmount);
 
       const contractState = {
         createAgendaFee,
